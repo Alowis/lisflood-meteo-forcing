40f902831ced0bf528184de21bf97b0b5ee01db7
Josias Láng-Ritter <ritterjosias@gmail.com>
2022-09-02 15:07:14 +0300

    Update ERA5land_CDS_downloader.py

--- ERA5land_CDS_downloader.py
+++ ERA5land_CDS_downloader.py
@@ -1,148 +1,315 @@
+
+"""
+Created on Mon Apr  4 17:09:58 2022
+
+@author: Alois Tilloy and Hylke Beck
+"""
+
+#!/usr/bin/env python  
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 14 10:50:54 2021
 
-@author: tilloal
-"""
+__author__ = "Alois Tilloy"
+__date__ = "June 2022"
 
-import cdsapi
-import requests
-import os
-from tools import *
 
-## Script 1/4
+import rioxarray # for the extension to load
+import os, sys, glob, time, pdb
+import pandas as pd
+import numpy as np
+import xarray as xr
 
-# CDS API script to use CDS service to retrieve hourly ERA5* variables and iterate over
-# all months in the specified years.
- 
-# Requires:
-# 1) the CDS API to be installed and working on your system
-# 2) You have agreed to the ERA5 Licence (via the CDS web page)
-# 3) Selection of required variable, daily statistic, etc
- 
-# Output:
-# 1) separate netCDF file for chosen variables at hourly resolution for each month
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 # Change working directory
 os.chdir(dir_path)
 
-config = load_config("cds_config.cfg")
-# key text file containing (1) proxy key, (2) CDS API key, (3) working directory of the data
+#from config_comp import *
+import netCDF4 as nc
+from tools import *
+from skimage.transform import resize
+from skimage.transform import rescale
+from skimage.io import imread
+import matplotlib.pyplot as plt
+#import rasterio
+import shutil
+from datetime import timedelta
 
-# Proxy settings (comment this part if your system does not use a proxy)
-#proxy = config ['proxyKey']
-#os.environ['http_proxy'] = proxy 
-#os.environ['HTTP_PROXY'] = proxy
-#os.environ['https_proxy'] = proxy
-#os.environ['HTTPS_PROXY'] = proxy
+# Load configuration file 
+config = load_config(sys.argv[1])
+namefiles=config['namefiles']
+petc = config['petc']
+compression = config['compression']
 
-CDSAPI_URL="https://cds.climate.copernicus.eu/api/v2"
-# Load personnal API Key
-CDSAPI_KEY=config['CDSAPI_KEY']
-c = cdsapi.Client(key=CDSAPI_KEY, url=CDSAPI_URL, timeout=1500, quiet=False, debug=True)
+eu_area = [72.25, -25.25, 22.25, 50.25,]
+e5land_res= config['e5land_res']
+cover = config['cover']
+start=  int(sys.argv[2])
+end = int(sys.argv[3])
 
-# select variable(s); name must be a valid ERA5 CDS API name.
-# Here I select varaibles necessary to compute evapotranspiration
-input_string = input('Enter varialbes that need to be processed from the list [u10,v10,2t,str,ssrd,d2m], enter "all" for all variables')
-print("\n")
-vin = input_string.split()
-if vin == ["all"]:
-    varnames = ['u10', 'v10','2t','str','ssrd','2d','tp']
-else:
-    varnames=vin
+#start=1987
+#end=1988
+print("working on " + cover)
+
+if compression=='1':
+    print('netcdf files will be compressed using add_offset and scale_factor')
+
+def main():
+
+    # Output dates
+    year_start,year_end = start, end
+    out_dates_dly = pd.date_range(start=datetime(year_start,1,1), end=datetime(year_end+1,1,1)-pd.Timedelta(days=1), freq='D')
     
-# print list
-print('list: ', varnames)
+    # Load template map
+    dset = nc.Dataset(config['templatemap_path'])
+    dset.set_auto_maskandscale(False)
 
+    template_lat = np.asarray(dset.variables['lat'])
+    template_lon = np.asarray(dset.variables['lon'])
+    template_res = round(template_lon[1]-template_lon[0],13)
+    varname = list(dset.variables.keys())[-1]
+    template_np = np.array(dset.variables[varname][:])
+    condition = template_np==0
 
-#this list has to be modified to include more variables
-pair_var=[['u10', 'v10','2t','str','ssrd','2d','tp'],
-          ['10m_u_component_of_wind', '10m_v_component_of_wind', '2m_temperature','surface_net_thermal_radiation', 'surface_solar_radiation_downwards','2m_dewpoint_temperature','total_precipitation']]
-
-pair_match=set(pair_var[0]).intersection(varnames)
-nf = [item for item,x in enumerate(pair_var[0]) if x in varnames]
-
-var=[pair_var[1][i] for i in nf]
-
-print('variable(s) '+' | '.join(var) + ' will be downloaded')
-
-                                        #,'surface_net_thermal_radiation', 'surface_net_solar_radiation','2m_dewpoint_temperature']
-#var = ['10m_u_component_of_wind', '10m_v_component_of_wind', '2m_temperature','surface_net_thermal_radiation', 'surface_net_solar_radiation','2m_dewpoint_temperature']
-#varnames = ['u10', 'v10','2t','str','ssrd','2d']
-date = config['date']
-
-
-# define area where data needs to be extracted (long/lat)
-area=[config['Lat1'], config['Lon1'], config['Lat2'], config['Lon2'],]
-
-#automatic selection of date range according to user input
-if date == 'auto':
-    start=  int(sys.argv[1])
-    end = int(sys.argv[2])
-    out_Yrs = list(range(start,end+1))
-    years = out_Yrs
-
-#manual year selection
-if date == "manual":
-    # Uncomment years as required
-    years =  [
-    #'1981','1982', '1983','1984',
-    #'1985', '1986', 'u101987',
-    '1988',
-    #'1989', '1990',
-    #'1991', '1992', '1993','1994', '1995', '1996','1997', '1998',
-    #'1999',
-    #'2000', '2001', '2002','2003', '2004', '2005','2006', '2007', '2008',
-    #'2009', '2010', '2011','2012', '2013', '2014','2015', '2016', '2017',
-    #'2018', '2019', '2020',
+    # Determine map sizes
+    mapsize_global = (np.round(180/template_res).astype(int),np.round(360/template_res).astype(int))
+   
+    # this is the map size for pan-european hydrological anaysis
+    scaleR=(0.1/template_res)
     
-    ]
+    # The grid need to be shifted if border has 2 significan digits
+    shift=round(eu_area[0]-round(eu_area[0],1),2)
+    mapsize_ereu =((np.round((eu_area[0]-eu_area[2])/e5land_res)+1).astype(int),(np.round((eu_area[3]-eu_area[1])/e5land_res)+1).astype(int))
+    size_eulon = round(mapsize_ereu[0]*scaleR)
+    size_eulat = round(mapsize_ereu[1]*scaleR)
+    mapsize_europe = (size_eulon,size_eulat)
+    mapsize_europe2 = (np.round((eu_area[0]-eu_area[2])/template_res).astype(int),np.round((eu_area[3]-eu_area[1]+1)/template_res).astype(int))
+    row_ue,col_lue = latlon2rowcol(template_lat[0],template_lon[0],template_res,eu_area[0]+shift,eu_area[1]-shift)
+
+    mapsize_template = template_np.shape
+
+    # locate the european and template domain in the global domain
+    row_upper,col_left = latlon2rowcol(template_lat[0],template_lon[0],template_res,90,-180)
+    row_uppeu,col_lefeu = latlon2rowcol(eu_area[0]+shift,eu_area[1]-shift,template_res,90,-180)
+
+    # Load elevation data, append zeros to top and bottom to make global, and resample to template resolution
+    elev = np.zeros((21600,43200),dtype=np.single)
+
+    srcz=imread(os.path.join(config['dem_folder'],'elevation_1KMmn_GMTEDmn.tif'), plugin="tifffile")
+    
+    # Removed rastrio because it is conflicting with xarray
+    #src = rasterio.open(os.path.join(config['dem_folder'],'elevation_1KMmn_GMTEDmn.tif'))
+
+    elev[720:17520,:] = srcz
+
+    elev_global = imresize_mean(elev,mapsize_global)
+    elev_europe = elev_global[row_uppeu:row_uppeu+mapsize_europe[0],col_lefeu:col_lefeu+mapsize_europe[1]]
+    elev_template = elev_global[row_upper:row_upper+len(template_lat),col_left:col_left+len(template_lon)]
+
+    # Prepare temperature downscaling
+    tmp = imresize_mean(elev_global,(1800,3600)) # Resample to dimensions of input data - global
+    
+    #Rescale to output resolution
+    tmp = rescale(tmp,scaleR,order=1,mode='edge',anti_aliasing=False)
+
+    #Add option: "Europe" vs "Global"
+    if cover=="global":
+        elev_delta = elev_global-tmp
+    if cover=="europe":
+        tmp=tmp[row_uppeu:row_uppeu+mapsize_europe[0],col_lefeu:col_lefeu+mapsize_europe[1]]
+        elev_delta = elev_europe-tmp
+    #plt.imshow(temp_delta,vmin=-0.5,vmax=0.5)
+    #plt.show()
+
+    temp_delta = -6.5*elev_delta/1000 # Simple 6.5 degrees C/km lapse rate
+    lat_global = np.repeat(np.resize(np.arange(90-template_res/2,-90-template_res/2,-template_res),(10800,1)),mapsize_global[1],axis=1)
+    lat_template = lat_global[row_upper:row_upper+len(template_lat),col_left:col_left+len(template_lon)]
+    lat_europe = lat_global[row_uppeu:row_uppeu+mapsize_europe[0],col_lefeu:col_lefeu++mapsize_europe[1]]
+   
+    # Check if output already exists in scratch folder or output folder
+    scratchoutdir = os.path.join(config['scratch_folder'],'e5land_reanalysis')
+    finaloutdir = os.path.join(config['output_folder'],'e5land_reanalysis')
+        
+    # Check if all input variables are present
+    varnames = ['ws', 'ta','td','rn','rgd']
+    nfiles = {}
+    for vr in varnames:
+        files = glob.glob(os.path.join(config['e5land_folder'], namefiles + "_ta_*.nc"))  
+        nfiles[vr] = len(files)
+    nfiles_arr = np.array(list(nfiles.values()))
+    if (np.max(nfiles_arr)==0) | (any(nfiles_arr<np.max(nfiles_arr))):
+        print(nfiles)
+        raise Exception('Not all input variables present')
+        sys.exit()
+
+    # Initialize output files
+    if os.path.isdir(scratchoutdir)==False:
+        os.makedirs(scratchoutdir)
+    ncfile_ta = initialize_netcdf(os.path.join(scratchoutdir,'ta.nc'),template_lat,template_lon,'ta','degree_Celsius',compression,1)
+    ncfile_pr = initialize_netcdf(os.path.join(scratchoutdir,'tp.nc'),template_lat,template_lon,'tp','mm d-1',compression,1)
+    
+    if petc=="1":
+        ncfile_et = initialize_netcdf(os.path.join(scratchoutdir,'et.nc'),template_lat,template_lon,'et','mm d-1',compression,1)
+        ncfile_ew = initialize_netcdf(os.path.join(scratchoutdir,'ew.nc'),template_lat,template_lon,'ew','mm d-1',compression,1)
+        ncfile_es = initialize_netcdf(os.path.join(scratchoutdir,'es.nc'),template_lat,template_lon,'es','mm d-1',compression,1)
+    else: 
+        ncfile_rn = initialize_netcdf(os.path.join(scratchoutdir,'rn.nc'),template_lat,template_lon,'rn','J m-2 d',compression,1)
+        ncfile_ws = initialize_netcdf(os.path.join(scratchoutdir,'ws.nc'),template_lat,template_lon,'ws','m s-1',compression,1)
+        ncfile_rgd = initialize_netcdf(os.path.join(scratchoutdir,'rgd.nc'),template_lat,template_lon,'rgd','J m-2 d',compression,1)
+        ncfile_td = initialize_netcdf(os.path.join(scratchoutdir,'td.nc'),template_lat,template_lon,'td','degree_Celsius',compression,1)
+
+    # Loop over input files (MFDataset doesn't work properly)
+    print(os.path.join(config['e5land_folder'],namefiles + "_ta_*.nc"))
+    files = glob.glob(os.path.join(config['e5land_folder'], namefiles + "_ta_*.nc"))
+    
+
+    for file in files:
+
+        splitname=os.path.basename(file).split('_')
+        yrloc=len(splitname)-1 #the year is always at the end of the filename
+        file_yearnc = os.path.basename(file).split('_')[yrloc]
+        file_year = int(file_yearnc.split('.')[0])
+        #file_year_end = int(os.path.basename(file).split('_')[6][:-3])
+        file_dates_dly = pd.date_range(start=datetime(file_year,1,1), end=datetime(file_year+1,1,1)-pd.Timedelta(days=1), freq='D')
+        hits = np.sum((out_dates_dly.year==file_year))
+        if hits==0:
+          continue
+
+        # Open input files
+        print('Processing '+os.path.basename(file))
+        t0 = time.time()
+        dset_tmean = xr.open_dataset(file,diskless=True) # degrees C
+# JLR: REVISE FROM HERE, ESPECIALLY LINE BELOW
+        dset_wind = xr.open_dataset(file.replace('ta','ws'),diskless=True) # m/s
+        dset_tdew = xr.open_dataset(file.replace('ta','td'),diskless=True) # Pa
+        dset_swd = xr.open_dataset(file.replace('ta','rgd'),diskless=True) # W/m2
+        dset_lwd = xr.open_dataset(file.replace('ta','rn'),diskless=True) # W/m2
+        dset_pr = xr.open_dataset(file.replace('ta','tp'),diskless=True) # mm/d
+        
+        # Loop over days of input file
+        for ii in np.arange(len(file_dates_dly)):
+
+            if file_dates_dly[ii] not in out_dates_dly:
+                continue
+   
+            print('Processing year '+ str(file_year) +', date: '+str(file_dates_dly[ii])+', time stamp: '+datetime.now().strftime("%d/%m/%Y, %H:%M:%S")+')')                    
+
+            # Read data from input files
+            data = {}
+            #index = np.where(out_dates_dly==file_dates_dly[ii])[0][0]                            
+            data['ta'] = dset_tmean['ta'][ii,:,:] # degrees C
+            if petc=="1":
+               data['ws'] = dset_wind.variables['ws'][ii,:,:]*0.75 # m/s (factor 0.75 to translate from 10-m to 2-m height)
+            else:
+                data['ws'] = dset_wind['ws'][ii,:,:] # m/s (the factor will be applied in LISVAP)
+                data['rgd'] = dset_swd['rgd'][ii,:,:] # J/m2/d
+                data['rn'] = dset_lwd['rn'][ii,:,:] # J/m2/d
+                data['tp'] = dset_pr['tp'][ii,:,:] # mm/d
+                data['td'] = dset_tdew['td'][ii,:,:]
+                
  
-months = [ "01", "02", "03", "04", "05", "06", 
-         "07", "08", "09", "10", "11", "12" ] 
+            # Simple lapse rate downscaling of temperature and air pressure, nearest-neighbor resampling of other vars
+            for key in data.keys():
+                data[key]=data[key].rio.write_crs(4326)
+                data[key]=data[key].rio.write_nodata('nan')
+                data[key]=data[key].rio.set_spatial_dims('lon','lat')
+                data[key]=data[key].rio.interpolate_na(method='nearest')
+  
+                if key=='xxx': #(key=='ta')| (key=='td'): #JLR
+                    data[key] = resize(data[key],mapsize_europe,order=1,mode='constant',anti_aliasing=False)
+                    data[key] = np.around(data[key]+temp_delta,1)
+                else:
+                    #3 possible methods
+                    #data[key] = resize(data[key],mapsize_europe,order=0,mode='edge',anti_aliasing=False) 
+                    data[key] = rescale(data[key], scaleR, order=0, mode='edge', anti_aliasing=False)
+                    
+                    #If this method is used no need to subset data to template region
+                    #data[key] = data[key].interp_like(obj, method='nearest')
 
-# Choose directory where to download files
-dire=config["download_folder"]
-direh=dire + "/hourly"
-os.chdir(direh)
+            # Subset data to template region
+            for key in data.keys():
+                if cover=="global":
+                    row_upper = 0 #JLR
+                    col_left = 1 #JLR
+                    data[key] = data[key][row_upper:row_upper+len(template_lat),col_left:col_left+len(template_lon)]
+                if cover=="europe":
+                    data[key] = data[key][row_ue:row_ue+len(template_lat),col_lue:col_lue+len(template_lon)]
 
-# main loop to download variables
-# monthly files
-# all variables will be contained in one netcdf
-for yr in years:
-    for mn in months:
+        # fill the values with NA where condition is false, where there should be NaN
 
-        c.retrieve(
-        'reanalysis-era5-land',
-        {
-            'variable': var,
-            'year': yr,
-            'month': mn,
-            'day': [
-                '01', '02', '03',
-                '04', '05', '06',
-                '07', '08', '09',
-                '10', '11', '12',
-                '13', '14', '15',
-                '16', '17', '18',
-                '19', '20', '21',
-                '22', '23', '24',
-                '25', '26', '27',
-                '28', '29', '30',
-                '31',
-            ],
-            'time': [
-                '00:00', '01:00', '02:00',
-                '03:00', '04:00', '05:00',
-                '06:00', '07:00', '08:00',
-                '09:00', '10:00', '11:00',
-                '12:00', '13:00', '14:00',
-                '15:00', '16:00', '17:00',
-                '18:00', '19:00', '20:00',
-                '21:00', '22:00', '23:00',
-            ],
-            'area':area ,
-            'format': 'netcdf',
-        },
-        "e5l_lf-met" + "_" + yr + "_" + mn + ".nc")
+                data[key]= np.where(condition,np.nan,data[key])
+                
+            #compression part
+                if compression=="1":
+                    scale_factor=meteo_vars_config[key][KEY_SCALE_FACTOR]   
+                    add_offset=meteo_vars_config[key][KEY_OFFSET] 
+                    data[key][np.isnan(data[key])] = (-9999 - add_offset) * scale_factor
+
+         
+            # Write data to output netCDFs
+            time_value = (file_dates_dly[ii]-pd.to_datetime(datetime(1950, 1, 1))).total_seconds()/86400
+            index = np.where(out_dates_dly==file_dates_dly[ii])[0][0]
+
+            #Potential evapotranspiration is not implemented yet for the set of input from ERA5-land
+            #daily variables need to be the accumulation of the previous day
+            #no need to move date as the variable is already an accumulation of the previous day
+            
+            ncfile_pr.variables['time'][index] = time_value
+            ncfile_pr.variables['tp'][index,:,:] = data['tp']
+            
+            #+1 as LISVAP and LISFLOOD use the value of the previous day
+            ncfile_ta.variables['time'][index] = time_value+1
+            ncfile_ta.variables['ta'][index,:,:] = data['ta']
+      
+            ncfile_ws.variables['time'][index] = time_value+1 
+            ncfile_ws.variables['ws'][index,:,:] = data['ws']
+            ncfile_rn.variables['time'][index] = time_value
+            ncfile_rn.variables['rn'][index,:,:] = data['rn']
+            ncfile_rgd.variables['time'][index] = time_value
+            ncfile_rgd.variables['rgd'][index,:,:] = data['rgd']
+            ncfile_td.variables['time'][index] = time_value+1
+            ncfile_td.variables['td'][index,:,:] = data['td']
+                    
+            # Generate figures to verify output
+            if ii==0:
+                makefig('figures','ta',data['ta'],0,12)
+                #makefig('figures','pr',data['pr'],0,12)
+                for key in data.keys():
+                    makefig('figures',key,data[key],np.min(data[key]),np.max(data[key]))
+                makefig('figures','elev_template',elev_template,0,6000)
+        
+        # Close input files
+        dset_tmean.close()
+        dset_wind.close()
+        dset_swd.close()
+        dset_lwd.close()
+        dset_pr.close()
+        dset_tdew.close()
+        
+
+        print("Time elapsed is "+str(time.time()-t0)+" sec") 
+    # Close output files
+    ncfile_pr.close()
+    ncfile_ta.close()
+    
+    if "petc"==1: 
+        ncfile_et.close()
+        ncfile_ew.close()
+        ncfile_es.close()
+    else:  
+        ncfile_ws.close()
+        ncfile_rn.close()
+        ncfile_rgd.close()
+        ncfile_td.close()
+    # Move output from scratch folder to output folder
+    print('-------------------------------------------------------------------------------')
+    if os.path.isdir(finaloutdir)==False:
+        os.makedirs(finaloutdir)
+    for file in glob.glob(os.path.join(scratchoutdir,'*')):
+        t0 = time.time()
+        print('Moving '+os.path.basename(file)+' ('+str(round(os.path.getsize(file)/10**9))+' GB) to '+finaloutdir)
+        shutil.copy(file, finaloutdir)
+        print("Time elapsed is "+str(time.time()-t0)+" sec")
+    shutil.rmtree(scratchoutdir)
+
+if __name__ == '__main__':
+    main()
